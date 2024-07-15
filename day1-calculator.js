d4de951520898da5457e71ad009a09aea11920c7
a-raheem-27 <a.raheem.27ar@gmail.com>
2024-07-15 18:19:54 +0500

    It's a faulty calculator that gives 90% right answer and 10% wrong

---
+++ day1-calculator.js
@@ -0,0 +1,61 @@
+let a = +prompt("Enter first number");
+let b = +prompt("Enter second number");
+let c = prompt("Which operation you want to perform? Enter 1 for Addition, 2 for subtraction, 3 for multiplication, 4 for division and 5 for modulus")
+
+
+function calculator(x,y,z){
+    switch(z){
+        case "1":
+            if(Math.random()<0.1){
+                alert(x*y);
+                break;
+            }
+            else{
+                alert(x+y);
+                break;
+            }
+
+        case "2":
+            if(Math.random()<0.1){
+                alert(x/y);
+                break;
+            }
+            else{
+                alert(x-y);
+                break;
+            }
+
+        case "3":
+            if(Math.random()<0.1){
+                alert(x+y);
+                break;
+            }
+            else{
+                alert(x*y);
+                break;
+            }
+
+        case "4":
+            if(Math.random()<0.1){
+                alert(x**y);
+                break;
+            }
+            else{
+                alert(x/y);
+                break;
+            }
+
+        case "5":
+            if(Math.random()<0.1){
+                alert(x/y);
+                break;
+            }
+            else{
+                alert(x**y);
+                break;
+            }
+    }
+}
+
+
+calculator(a,b,c);
